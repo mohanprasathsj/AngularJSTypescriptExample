74bb4677aca809862fc4b4f397f9e5e43f1bc8c3
Mohan Prasath <mohanprasathsj@outlook.com>
2017-11-29 11:05:38 +0800

    dsfdsf

--- AngularJSTypescriptExample/App_Start/BundleConfig.cs
+++ AngularJSTypescriptExample/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace AngularJSTypescriptExample
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version1}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
